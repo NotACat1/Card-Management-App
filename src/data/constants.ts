bb86d3863d12780338e9f4622c9e53e030a0ddfc
NotACat1 <notacat.mail@gmail.com>
2024-08-24 21:05:27 +0300

    feat: создание константных данных

---
+++ src/data/constants.ts
@@ -0,0 +1,2 @@
+export const UNSPLASH_URL = 'https://api.unsplash.com/';
+export const MAX_PAGES = 4;
---
+++ src/data/contacts.ts
@@ -0,0 +1,6 @@
+export const contacts = {
+  phone: '+79672482206',
+  email: 'notacat.mail@gmail.com',
+  github: 'https://github.com/NotACat1',
+  telegram: 'https://t.me/NotACat1',
+};
